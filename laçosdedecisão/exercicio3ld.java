de58a58af1d9dccb10eade411e07c8820c7377f7
unknown <rafaelsdeliborio@gmail.com>
2021-06-09 09:01:52 -0300

    versao1

---
+++ laçosdedecisão/exercicio3ld.java
@@ -0,0 +1,10 @@
+package laçosdedecisão;
+
+public class exercicio3ld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ laçosdedecisão/exercicio4ld.java
@@ -0,0 +1,10 @@
+package laçosdedecisão;
+
+public class exercicio4ld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
